cf72495dc3d46ab7fc4f23b78e48d725a824ffae
Razieh Abedi <raziyeabedi@gmail.com>
2022-09-15 15:02:12 +0800

    created the thirs section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,27 +3,29 @@ import profile from "../assets/images/profile-pic.jpg";
 
 function About() {
   return (
-    <div className="my-5">
-       <h2 className="text-center"><span className="number">01. </span><span>About Me</span></h2>
-       <div className="container">
-      <div className="row align-items-center justify-content-between">
-        <div className="col col-8 about-col">
-          <h6>Hi, my name is</h6>
-          <h1 className="ps-5 display-1">Razieh Abedi</h1>
-          <h2 className="pt-3">I'm a web developer.</h2>
-          <p className="w-75 pt-3">
-            I’m a software engineer specializing in building (and occasionally
-            designing) exceptional digital experiences. Currently, I’m focused
-            on building accessible, human-centered products at Upstatement.
-          </p>
-        </div>
-        <div className="col col-4 about-col">
-          <img src={profile} alt="Razi" className="h-100 w-100 rounded" />
+    <section className="vh-100">
+      <h2 className="text-center">
+        <span className="number">01. </span>
+        <span>About Me</span>
+      </h2>
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+          <div className="col col-8 about-col">
+            <h6>Hi, my name is</h6>
+            <h1 className="ps-5 display-1">Razieh Abedi</h1>
+            <h2 className="pt-3">I'm a web developer.</h2>
+            <p className="w-75 pt-3">
+              I’m a software engineer specializing in building (and occasionally
+              designing) exceptional digital experiences. Currently, I’m focused
+              on building accessible, human-centered products at Upstatement.
+            </p>
+          </div>
+          <div className="col col-4 about-col">
+            <img src={profile} alt="Razi" className="h-100 w-100 rounded" />
+          </div>
         </div>
       </div>
-    </div>
-    </div>
-   
+    </section>
   );
 }
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 function Work() {
   return (
-    <div className='container my-5'>
-        <h2>03. Some things I have built</h2>
-        <div>projects</div>
-    </div>
-  )
+    <section className="vh-100">
+      <h2 className="text-center">
+        <span className="number">02. </span>
+        <span>Some works I have done</span>
+      </h2>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card project">
+              <div className="card-body">
+                <h5 className="card-title text-black">Shopping Website</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Headphone shopping
+                </h6>
+                <p className="card-text text-black">
+                  This is a sample shopping website with a shopping cart.
+                </p>
+                <a
+                  href="https://razieh-abedi.github.io/shopping-cart/"
+                  class="card-link"
+                >
+                  <FaExternalLinkAlt />
+                </a>
+                {/* <a href="#" class="card-link">
+                  Another link
+                </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Work
+export default Work;
