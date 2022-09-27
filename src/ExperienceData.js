fb64ed7360e04662992be7352f6ecaf11b39a568
Razieh Abedi <raziyeabedi@gmail.com>
2022-09-27 17:26:38 +0800

    added the experience section

---
+++ src/ExperienceData.js
@@ -0,0 +1,40 @@
+const ExperienceData = [
+    {
+      id: 1,
+      company: "Company 1",
+      dates: "2010-2015",
+      title: "title 1",
+      duties: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+        
+      ],
+    },
+    {
+      id: 2,
+      company: "Company 2",
+      dates: "2010-2015",
+      title: "title 1",
+      duties: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+       
+      ],
+    },
+    {
+      id: 3,
+      company: "Company 3",
+      dates: "2010-2015",
+      title: "title 1",
+      duties: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+        
+      ],
+    },
+  ];
+  export default ExperienceData;
+  
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 // import { useParams } from "react-router-dom";
+import ExperienceData from "../ExperienceData";
+import { TiChevronRight } from "react-icons/ti";
 
 function Experience() {
   // const { experience } = useParams;
+  const [value, setValue] = useState(0);
+  const { company, dates, duties, title } = ExperienceData[value];
   return (
-    <section className="py-2 py-md-5" id="experience">
+    <section className="py-2 py-md-5">
       <h2 className="text-center  mb-3 mb-md-5">
         <span className="number">02. </span>
         <span>Some works I have done</span>
       </h2>
-      <div className="container px-4">
-        <div className="row">
-          <div className="col">
-            <ul>
-              <li>work 1</li>
-              <li>work2</li>
-              <li>work3</li>
-            </ul>
-          </div>
+      <main className="d-md-flex container justify-content-between">
+        <div className="btnContainer w-25 py-4 ">
+          {ExperienceData.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`mt-3 mx-auto btn btn-outline-light d-block jobBtn ${
+                  index === value ? "activeJobBtn" : ""
+                }`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
         </div>
-      </div>
+        <div className="w-75 ">
+          <h3>{title}</h3>
+          <h5 className="btn btn-outline-light companyBtn">{company}</h5>
+          <p className="text-black-50">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div
+                key={index}
+                className="d-flex align-items-center justify-content-start"
+              >
+                <TiChevronRight className="jobIcon me-2" />
+                <p className="jobInfo">{duty}</p>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </section>
   );
 }
