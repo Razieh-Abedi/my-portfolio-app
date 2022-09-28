bedd4f8ec0bb288be67f4176cdb3069a3453587c
Razieh Abedi <raziyeabedi@gmail.com>
2022-09-28 18:14:27 +0800

    modified the about section:

--- src/ExperienceData.js
+++ src/ExperienceData.js
@@ -1,40 +1,36 @@
 const ExperienceData = [
-    {
-      id: 1,
-      company: "Company 1",
-      dates: "2010-2015",
-      title: "title 1",
-      duties: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-        
-      ],
-    },
-    {
-      id: 2,
-      company: "Company 2",
-      dates: "2010-2015",
-      title: "title 1",
-      duties: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-       
-      ],
-    },
-    {
-      id: 3,
-      company: "Company 3",
-      dates: "2010-2015",
-      title: "title 1",
-      duties: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-        
-      ],
-    },
-  ];
-  export default ExperienceData;
-  
+  {
+    id: 1,
+    company: "Self-employed",
+    dates: "2021-2022",
+    title: "Freelance Web Developer",
+    duties: [
+      "Created a website for a hotel/restuarant company.",
+      "Created web mini applications using JavaScript,and React hooks.",
+      "Launced the front end of an education website. ",
+    ],
+  },
+  {
+    id: 2,
+    company: "Company 2",
+    dates: "2010-2015",
+    title: "title 1",
+    duties: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+    ],
+  },
+  {
+    id: 3,
+    company: "Company 3",
+    dates: "2010-2015",
+    title: "title 1",
+    duties: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+    ],
+  },
+];
+export default ExperienceData;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import profile from "../assets/images/profile-pic.jpg";
-
+import { TiChevronRight } from "react-icons/ti";
 function About() {
   return (
-    <section className="py-2 py-md-5" id="about">
+    <section className="py-2 py-md-5">
       <h2 className="text-center  mb-3 mb-md-5">
         <span className="number">01. </span>
         <span>About Me</span>
@@ -14,11 +14,66 @@ function About() {
             <h6>Hi, my name is</h6>
             <h1 className="ps-5 display-1">Razieh Abedi</h1>
             <h2 className="pt-3">I'm a web developer.</h2>
+
             <p className="p-3">
-              I’m a software engineer specializing in building (and occasionally
-              designing) exceptional digital experiences. Currently, I’m focused
-              on building accessible, human-centered products at Upstatement.
+              I’m a certified, determined, and productive front-end web
+              developer with a passion for creative solutions. I develope,
+              create, and design responsive websites and web applications using
+              the following technologies:
             </p>
+            <div className="d-flex justify-content-between">
+              <div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-1" />
+                  React.js (hooks)
+                </div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  JavaScript
+                </div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  HTM5
+                </div>
+              </div>
+              <div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  CSS3
+                </div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  GIT
+                </div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  BootStrap5
+                </div>
+              </div>
+              <div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  Visual studio Code
+                </div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  WordPress
+                </div>
+                <div>
+                  {" "}
+                  <TiChevronRight className="jobIcon me-2" />
+                  PhotoShop
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col col-12 col-md-4 about-col">
             <img src={profile} alt="Razi" className="h-100 rounded" />
